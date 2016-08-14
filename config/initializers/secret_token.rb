d956edd9e10e61d33a733603b3588cad37db1550
Marcos Martins <marcos.martins2307@gmail.com>
2016-08-13 22:13:40 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yellowphant::Application.config.secret_token = 'd2299c0154dd92c47e609e8cd2ec546d4f4cec7f0750286a7158c4145ec8fdd3ad04888677608c7b818048966490cca18fa91e841453c168a67694aa1a108252'
